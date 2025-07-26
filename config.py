c7f320701e855ad16d8d7944a7cacc4323680e4e
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-07-26 23:09:03 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+API_TOKEN = 'YOUR_BOT_TOKEN_HERE'  # Replace with your bot token
+API_ID = 123456  # Replace with your Telethon API ID
+API_HASH = 'YOUR_API_HASH_HERE'  # Replace with your API hash
+OWNER_USERNAME = '@siyorou'
