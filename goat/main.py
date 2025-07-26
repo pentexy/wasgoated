b747f76fed22369d281ce21fb64d6a9e9743c8c4
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-07-26 23:08:13 +0530

    Create main.py

---
+++ goat/main.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils import executor
+from config import API_TOKEN
+from handlers import register_handlers
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+register_handlers(dp)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
