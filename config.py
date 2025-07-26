fe4990586a39b907365728e9076cc862aca543c2
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-07-26 23:20:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_TOKEN = 'YOUR_BOT_TOKEN_HERE'  # Replace with your bot token
-API_ID = 123456  # Replace with your Telethon API ID
-API_HASH = 'YOUR_API_HASH_HERE'  # Replace with your API hash
+API_TOKEN = '8185730588:AAHkdJRV9ghuRb7M10sGnL5_8wW2SAypuuo'  # Replace with your bot token
+API_ID = 26416419  # Replace with your Telethon API ID
+API_HASH = 'c109c77f5823c847b1aeb7fbd4990cc4'  # Replace with your API hash
 OWNER_USERNAME = '@siyorou'
